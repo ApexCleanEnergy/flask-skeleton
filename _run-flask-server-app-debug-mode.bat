3f8b0d1e984191b4d79b09abd099c79f41990eb9
Silas Frantz <64056700+s-frantz@users.noreply.github.com>
2021-12-07 16:27:29 -0500

    Update _run-flask-server-app-debug-mode.bat

--- _run-flask-server-app-debug-mode.bat
+++ _run-flask-server-app-debug-mode.bat
@@ -1,3 +1,3 @@
 start chrome http://localhost:6969/
-"C:\PythonVenv\FlaskSkeleton\Scripts\python.exe" %~dp0run_flask_server.py --debug
+"C:\PythonVenv\FlaskSkeleton\Scripts\python.exe" %~dp0_run-flask-server.py --debug
 pause
